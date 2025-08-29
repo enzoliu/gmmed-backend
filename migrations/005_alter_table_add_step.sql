e25bf9aac10c12f27ee2222405f16b2c4dccea61
Enzo Liu <item.search@gmail.com>
2025-08-30 01:51:08 +0800

    fix: 增加drop table migration防呆機制

--- migrations/005_alter_table_add_step.sql
+++ migrations/005_alter_table_add_step.sql
@@ -1,4 +1,4 @@
 ALTER TABLE warranty_registrations ADD COLUMN IF NOT EXISTS step INT DEFAULT 0;
 CREATE INDEX IF NOT EXISTS idx_warranty_registrations_step ON warranty_registrations (step);
 
-ALTER TABLE products DROP CONSTRAINT chk_products_warranty_years_non_negative;
+ALTER TABLE products DROP CONSTRAINT IF EXISTS chk_products_warranty_years_non_negative;
